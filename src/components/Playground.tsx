09d47411989bee4256974604a862145e854329d9
walterlv <xblero@gmail.com>
2017-09-02 10:18:23 +0800

    Add empty project files

---
+++ src/components/Playground.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Playground.css';
+
+export default class Playground extends React.Component {
+    render() {
+        return (
+            <div className="Playground">
+                <div/>
+            </div>
+        );
+    }
+}
